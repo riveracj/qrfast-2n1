a9d566428f4c80f80dc1921214e622a855ac3b66
Chrisvie John Rivera <riveracjjanjan6@gmail.com>
2026-06-01 09:34:31 +0800

    Rename app to FastQR: QR code scanner w/ generator

--- lib/app.dart
+++ lib/app.dart
@@ -11,7 +11,7 @@ class QRFASTApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'QRFAST',
+      title: 'FastQR: QR code scanner w/ generator',
       theme: AppTheme.darkTheme,
       debugShowCheckedModeBanner: false,
       home: const MainShell(),
